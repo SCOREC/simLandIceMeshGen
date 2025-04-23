eb03af7da3fa39a0a6b46738f66d92aaed6f2637
Cameron Smith <smithc11@rpi.edu>
2025-04-22 21:54:41 -0400

    use simAdvModel

--- testSplineFitting.cc
+++ testSplineFitting.cc
@@ -1,9 +1,9 @@
-#include "MeshSim.h"
-#include "SimCreateModel.h"
-#include "SimDisplay.h"
-#include "SimInfo.h"
-#include "SimModel.h"
 #include "SimUtil.h"
+#include "SimModel.h"
+#include "SimInfo.h"
+#include "SimAdvModel.h"
+#include "SimDisplay.h"
+#include "MeshSim.h"
 #include <iostream>
 #include <math.h>
 #include <cassert>
@@ -14,10 +14,11 @@ using namespace std;
 
 void messageHandler(int type, const char *msg);
 int main(int argc, char **argv) {
-  pGImporter importer; // the importer object used to create the geometry
   pGVertex *vertices;  // array to store the returned model vertices
   pGEdge *edges;       // array to store the returned model edges
   pGFace *faces;       // array to store the returned model faces
+  pGRegion outerRegion;
+  pGIPart part;
   pGModel model;       // pointer to the complete model
 
   const auto prefix = std::string("foo");
@@ -34,7 +35,9 @@ int main(int argc, char **argv) {
     pProgress progress = Progress_new();
     Progress_setDefaultCallback(progress);
 
-    importer = GImporter_new();
+    model = GM_new(1);
+    part = GM_rootPart(model);
+    outerRegion = GIP_outerRegion(part);
 
     const auto numPts = 4;
     std::vector<double> xpts = {0, 4, 8, 12};
@@ -66,16 +69,13 @@ int main(int argc, char **argv) {
 
     vertices = new pGVertex[2];
     double startVtx[3] = {0, 0, 0};
-    vertices[0] = GImporter_createVertex(importer, startVtx);
+    vertices[0] = GR_createVertex(outerRegion, startVtx);
     double endVtx[3] = {12, 0, 0};
-    vertices[1] = GImporter_createVertex(importer, endVtx);
+    vertices[1] = GR_createVertex(outerRegion, endVtx);
 
     edges = new pGEdge[1];
-    edges[0] = GImporter_createEdge(importer, vertices[0], vertices[1], curve,
-                                    0, 1, 1);
+    edges[0] = GR_createEdge(outerRegion, vertices[0], vertices[1], curve, edgeDir);
 
-    // Now complete the model and delete the importer
-    model = GImporter_complete(importer);
     auto isValid = GM_isValid(model, 2, NULL);
     if (!isValid) {
       fprintf(stderr, "ERROR: model is not valid... exiting\n");
@@ -83,7 +83,6 @@ int main(int argc, char **argv) {
     } else {
       cout << "Model is valid.\n";
     }
-    GImporter_delete(importer);
 
     cout << "Number of vertices in model: " << GM_numVertices(model) << endl;
     cout << "Number of edges in model: " << GM_numEdges(model) << endl;
