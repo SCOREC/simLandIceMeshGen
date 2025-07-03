bf8da296e9ccab486f13874f0572b889753b7c7e
Cameron Smith <smithc11@rpi.edu>
2025-07-03 10:29:07 -0400

    test executes

---
+++ testCreateEdges.cc
@@ -0,0 +1,181 @@
+#include "landIceMeshGen.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm> // For std::find_if, std::erase
+#include <cctype>    // For std::isspace
+
+void messageHandler(int type, const char *msg);
+
+struct CurveInfo {
+    std::vector<double> x; 
+    std::vector<double> y;
+    std::vector<int> isOnCurve;
+    std::vector<int> isMdlVtx;
+};
+
+
+// Function to trim leading and trailing whitespace
+std::string trim(std::string s) {
+    // Trim leading whitespace
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+
+    // Trim trailing whitespace
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+    return s;
+}
+
+bool parseBool(const std::string& str) {
+    return (str == "1" || str == "true" || str == "True" || str == "TRUE");
+}
+
+template <typename T>
+void printVec(std::string name, std::vector<T>& vec) {
+  std::cerr << name << " "; 
+  for( auto v : vec ) std::cerr << v << " ";
+  std::cerr << "\n";
+}
+
+void printCurveInfo(CurveInfo& c) {
+  printVec("x", c.x);
+  printVec("y", c.y);
+  printVec("isOnCurve", c.isOnCurve);
+  printVec("isMdlVtx", c.isMdlVtx);
+}
+
+CurveInfo readCurveInfo(const std::string& filename) {
+  CurveInfo c;
+  std::ifstream file(filename);
+  std::string line;
+
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file: " << filename << std::endl;
+    return c;
+  }
+
+  // Read header
+  std::getline(file, line);
+
+  while (std::getline(file, line)) {
+    std::istringstream ss(line);
+    std::string token;
+    double ignored;
+    std::getline(ss, token, ','); c.x.push_back(std::stod(token));
+    std::getline(ss, token, ','); c.y.push_back(std::stod(token));
+    std::getline(ss, token, ','); ignored = std::stod(token); //z
+    std::getline(ss, token, ','); c.isOnCurve.push_back(parseBool(trim(token)));
+    std::getline(ss, token, ','); ignored = std::stod(token); //angle
+    std::getline(ss, token, ','); c.isMdlVtx.push_back(parseBool(trim(token)));
+  }
+
+  return c;
+}
+
+int main(int argc, char **argv) {
+  const int numExpectedArgs = 3;
+  if (argc != numExpectedArgs) {
+    std::cerr << "Usage: <input csv file> <output prefix>\n";
+    std::cerr << "input csv file with the following columns: "
+                 "x,y,z,isOnCurve,angle,isMdlVtx\n";
+    return 1;
+  }
+  assert(argc == numExpectedArgs);
+
+  std::string filename = argv[1];
+  const auto prefix = std::string(argv[2]);
+  std::cout << "input csv file: " << argv[1] << " "
+            << "output prefix: " << prefix << "\n";
+
+  std::string modelFileName = prefix + ".smd";
+  std::string meshFileName = prefix + ".sms";
+
+  const auto debug = true;
+
+  // You will want to place a try/catch around all SimModSuite calls,
+  // as errors are thrown.
+  try {
+    Sim_logOn("simMeshGen.log");
+    SimModel_start(); // Call before Sim_readLicenseFile
+    // NOTE: Sim_readLicenseFile() is for internal testing only.  To use,
+    // pass in the location of a file containing your keys.  For a release
+    // product, use Sim_registerKey()
+    Sim_readLicenseFile(0);
+    // Tessellation of GeomSim geometry requires Meshing to have started
+    MS_init();
+
+    Sim_setMessageHandler(messageHandler);
+    pProgress progress = Progress_new();
+    Progress_setDefaultCallback(progress);
+
+    ModelTopo mdlTopo;
+    mdlTopo.model = GM_new(1);
+    mdlTopo.part = GM_rootPart(mdlTopo.model);
+    mdlTopo.region = GIP_outerRegion(mdlTopo.part);
+
+    auto curveInfo = readCurveInfo(filename);
+    printCurveInfo(curveInfo);
+    GeomInfo geom;
+    geom.numVtx = curveInfo.isMdlVtx.size();
+    geom.vtx_x = curveInfo.x;
+    geom.vtx_y = curveInfo.y;
+    int firstContourPt = 0;
+    createEdges(mdlTopo, geom, curveInfo.isOnCurve, curveInfo.isMdlVtx,
+                debug, firstContourPt);
+
+    auto isValid = GM_isValid(mdlTopo.model, 2, NULL);
+    if (!isValid) {
+      fprintf(stderr, "ERROR: model is not valid... exiting\n");
+      exit(EXIT_FAILURE);
+    } else {
+      std::cout << "Model is valid.\n";
+    }
+
+    printModelInfo(mdlTopo.model);
+
+    GM_write(mdlTopo.model, modelFileName.c_str(), 0, 0);
+
+    // cleanup
+    GM_release(mdlTopo.model);
+    Progress_delete(progress);
+    MS_exit();
+    Sim_unregisterAllKeys();
+    SimModel_stop();
+    Sim_logOff();
+
+  } catch (pSimInfo err) {
+    std::cerr << "SimModSuite error caught:" << std::endl;
+    std::cerr << "  Error code: " << SimInfo_code(err) << std::endl;
+    std::cerr << "  Error string: " << SimInfo_toString(err) << std::endl;
+    SimInfo_delete(err);
+    return 1;
+  } catch (...) {
+    std::cerr << "Unhandled exception caught" << std::endl;
+    return 1;
+  }
+  return 0;
+}
+
+void messageHandler(int type, const char *msg) {
+  switch (type) {
+  case Sim_InfoMsg:
+    std::cout << "Info: " << msg << std::endl;
+    break;
+  case Sim_DebugMsg:
+    std::cout << "Debug: " << msg << std::endl;
+    break;
+  case Sim_WarningMsg:
+    std::cout << "Warning: " << msg << std::endl;
+    break;
+  case Sim_ErrorMsg:
+    std::cout << "Error: " << msg << std::endl;
+    break;
+  }
+  return;
+}
