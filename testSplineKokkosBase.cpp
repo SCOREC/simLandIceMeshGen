333c669cc0a2718d951fc6633605cd2113fbdc00
Hsieh <hsiehh@checkers.scorec.rpi.edu>
2026-03-10 23:08:50 -0400

    Fixed vector initialization error

--- testSplineKokkosBase.cpp
+++ testSplineKokkosBase.cpp
@@ -1,4 +1,4 @@
-#include "<Kokkos_Core.hpp>"
+#include <Kokkos_Core.hpp>
 #include "BSplineKokkos.h"
 #include <vector>
 
@@ -15,14 +15,13 @@ int main(int argc, char* argv[]) {
 	#define MemSpace Kokkos::HostSpace
 	#endif
 
-	using ExecSpace = MemSpace::execution_space;
+	using ExecutionSpace = MemSpace::execution_space;
 
 	//We will try to test with dummy data for BSplineKokkos creation
-	std::vector<double> dummyCtrlPts(2.14, 3.98, 1.56, 9.10, 11.87);
-	std::vector<double> dummyKnots(5.99, 8.98, 17.71, 21.1, 2.154, 9.17, 6.32);
+	std::vector<double> dummyCtrlPts{2.14, 3.98, 1.56, 9.10, 11.87};
+	std::vector<double> dummyKnots{5.99, 8.98, 17.71, 21.1, 2.154, 9.17, 6.32};
 	int dOrder = 2;
-	std::vector<double> dummyWeights(0.87, 0.125, 0.03);
-	BSplineKokkos ex1(dOrder, dummyCtrlPts, dummyKnots, dummyWeights);
-
-	
+	std::vector<double> dummyWeights{0.87, 0.125, 0.03};
+	BSplineKokkos<ExecutionSpace> ex1(dOrder, dummyCtrlPts, dummyKnots, dummyWeights);
+	Kokkos::finalize();
 }
