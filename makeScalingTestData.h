7d9b271378722a2ad8e880c97ab76746f0d458cc
hsiehhRPI <hsiehh@rpi.edu>
2026-08-06 11:02:46 -0400

    For generating data points from circle/ellipse

---
+++ makeScalingTestData.h
@@ -0,0 +1,13 @@
+#ifndef MAKESCALINGTESTDATA_H
+#define MAKESCALINGTESTDATA_H
+
+#include <Kokkos_Core.hpp>
+#include <vector>
+
+using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+using MemSpace = ExecutionSpace::memory_space;
+
+Kokkos::View<double*[2], MemSpace> makeCircle(double centerX, double centerY, double radius, int numPoints);
+Kokkos::View<double*[2], MemSpace> makeEllipse(double centerX, double centerY, double xRadius, double yRadius, int numPoints);
+
+#endif
